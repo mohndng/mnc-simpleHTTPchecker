0d7249a2b5f30fff4c606cac8634e99562d1a5ae
mohndng <93068579+mohndng@users.noreply.github.com>
2022-06-05 21:51:55 +0800

    Update mnchttpchecker.py

--- mnchttpchecker.py
+++ mnchttpchecker.py
@@ -4,6 +4,6 @@ from colorama import Fore, Back, Style
 
 response = requests.get("http://monding.hopto.org")
 
-print(Fore.YELLOW, Back.BLUE + "RESPONSE CODE")
+print(Fore.YELLOW + "Raymund is checking...")
 print(Style.RESET_ALL)
 print(Fore.GREEN, Style.BRIGHT, response.status_code)
