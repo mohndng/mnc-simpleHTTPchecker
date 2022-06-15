00e95d6658a1316db69e0f3ea7a1d800df74cb59
MNC Ozero <93068579+mohndng@users.noreply.github.com>
2022-06-15 22:31:29 +0800

    Update mnchttpchecker.py

--- mnchttpchecker.py
+++ mnchttpchecker.py
@@ -10,6 +10,6 @@ mon = requests.get('http://mncreation.cf',allow_redirects=False)
 
 time.sleep(0.5)
 print(Fore.RED, m); time.sleep(1)
-print(Fore.BLACK, n); time.sleep(4)
+print(Fore.BLUE, n); time.sleep(4)
 print(Fore.GREEN, c, Style.BRIGHT, mon.status_code)
 print(Style.RESET_ALL)
