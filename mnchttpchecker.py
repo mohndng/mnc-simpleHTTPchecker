eeba054ce987d5584d9df7bdf27b459ebfc1e681
mohndng <93068579+mohndng@users.noreply.github.com>
2022-06-15 21:59:42 +0800

    Update mnchttpchecker.py

--- mnchttpchecker.py
+++ mnchttpchecker.py
@@ -1,9 +1,15 @@
 import requests
+import time
 import colorama
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
-response = requests.get("http://monding.hopto.org")
+m = 'MnCHTTPCHECKER v0.5'
+n = 'Please wait for a while..'
+c = 'Success!'
+mon = requests.get('http://mncreation.cf',allow_redirects=False)
 
-print(Fore.YELLOW + "Raymund is checking...")
+time.sleep(0.5)
+print(Fore.RED, m); time.sleep(1)
+print(Fore.BLACK, n); time.sleep(4)
+print(Fore.GREEN, c, Style.BRIGHT, mon.status_code)
 print(Style.RESET_ALL)
-print(Fore.GREEN, Style.BRIGHT +  'HTTP/1.1',response.status_code)
