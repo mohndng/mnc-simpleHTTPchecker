8e56d348e985620ac76e4ec2816e73e8889e9a5c
MNC Ozero <93068579+mohndng@users.noreply.github.com>
2022-06-26 22:53:43 +0800

    Update and rename mnchttpchecker.py to mnchecker.py

--- mnchecker.py
+++ mnchecker.py
@@ -3,13 +3,13 @@ import time
 import colorama
 from colorama import Fore, Style
 
-m = 'MnCHTTPCHECKER v0.5'
+m = 'MnCHTTPCHECKER v0.8'
 n = 'Please wait for a while..'
 c = 'Success!'
 mon = requests.get('http://0.discoverapp.com',allow_redirects=False)
 
 time.sleep(0.5)
-print(Fore.RED, m); time.sleep(1)
+print(Fore.YELLOW, m); time.sleep(1)
 print(Fore.BLUE, n); time.sleep(4)
 print(Fore.GREEN, c, Style.BRIGHT, mon.status_code)
 print(Style.RESET_ALL)
